e3ad19268f813665b8b97165010f9d8edb4c6aed
DeniCoder <denis2042@yandex.ru>
2025-05-16 15:38:56 +0300

    add last change

--- movie_project/films/views.py
+++ movie_project/films/views.py
@@ -1,3 +1,18 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
+from .models import Film
 
-# Create your views here.
+def home(request):
+    return render(request, 'index.html')
+
+def feedback(request):
+    films = Film.objects.all()
+    return render(request, 'films/feedback.html', {'films': films})
+
+def new_feedback(request):
+    if request.method == 'POST':
+        title = request.POST.get('title')
+        description = request.POST.get('description')
+        review = request.POST.get('review')
+        Film.objects.create(title=title, description=description, review=review)
+        return redirect('feedback')
+    return render(request, 'films/new_feedback.html')
